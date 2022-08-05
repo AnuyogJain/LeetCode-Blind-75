1715b6135f6c1e15b0c4232578604ea32114ad7a
AnuyogJain <65466830+AnuyogJain@users.noreply.github.com>
2022-08-06 01:33:23 +0530

    Create group-anagrams.cpp

---
+++ group-anagrams.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    //generating a key 
+    string getKey(string str) {
+        vector<int> count(26);
+        for (int j = 0; j < str.size(); j++) {
+            count[str[j] - 'a']++;
+        }
+        
+        string key = "";
+        for (int i = 0; i < 26; i++) {
+            key.append(to_string(count[i] + 'a'));
+        }
+        return key;
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        vector<vector<string>> ans;
+        
+        for (int i = 0; i < strs.size(); i++) {
+            string key = getKey(strs[i]);
+            m[key].push_back(strs[i]);
+        }
+
+        for (auto it: m) {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
